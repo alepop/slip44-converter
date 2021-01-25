5d441502883712388240b83d53381ae30077954e
Aleksey Popov <alepooop@gmail.com>
2021-01-25 20:11:12 +0300

    chore(package): update bip44-constants package

--- index.js
+++ index.js
@@ -6,29 +6,27 @@ const constants = require('bip44-constants');
 const HARDENED_OFFSET = 0x80000000;
 
 const toBIP44 = address => {
+  try {
     const [coin, ...other] = address.split('/');
-    const constant = constants[coin.toUpperCase()];
-    if (constant) {
-        return `m/44'/${constant - HARDENED_OFFSET}'/${other.join('/')}`;
-    } else {
-        throw Error('Wrong coin symbol');
-    }
+    const [constant] = constants.find(([constant, coinSymbol]) => coinSymbol.toUpperCase() === coin.toUpperCase());
+    return `m/44'/${constant - HARDENED_OFFSET}'/${other.join('/')}`;
+  } catch {
+    throw Error('Wrong coin symbol');
+  }
 };
 
 const fromBIP44 = address => {
+  try {
     const [m, purpose, coin, ...other] = address.split('/');
-    const constant = parseInt(coin.replace("'", ''), 10) + HARDENED_OFFSET;
-    const [symbol] = Object.entries(constants).find(
-        ([key, val]) => val === constant,
-    );
-    if (symbol) {
-        return `${symbol}/${other.join('/')}`;
-    } else {
-        throw Error('Wrong address');
-    }
+    const parsedConstant = parseInt(coin.replace("'", ''), 10) + HARDENED_OFFSET;
+    const [_, coinSymbol] = constants.find(([constant]) => constant === parsedConstant);
+    return `${coinSymbol}/${other.join('/')}`;
+  } catch {
+    throw Error('Wrong address');
+  }
 };
 
 module.exports = {
-    toBIP44,
-    fromBIP44
+  toBIP44,
+  fromBIP44
 };
